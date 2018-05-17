363e89e7424cb897f6c69d82bb186ef46c6e91aa
Abhishek Raj <rai.abhishekraj@gmail.com>
2018-05-17 10:18:33 +0530

    done

--- db/db.js
+++ db/db.js
@@ -1,24 +1,9 @@
 const Sequelize = require('sequelize');
-
-
-const HEROKU_POSTGRESQL_BRONZE_URL = "postgres://yoeebvojbpcnth:7b265dce47d670c1e3e02c2a3d7286327673596ad76eed6b56ff7f556db0e04c@ec2-54-204-39-46.compute-1.amazonaws.com:5432/dbdfdcbssnmcnk";
-var db;
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-     db = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        port: 5432,
-        host: "ec2-54-204-39-46.compute-1.amazonaws.com",
-        logging: true
-    })
-} else {     
-     db = new Sequelize('learningsystem', 'root', 'root', {
+db = new Sequelize('learningsystem', 'root', 'root', {
         host: 'localhost',
         dialect: 'sqlite',      
           storage: './lms.db'
-    })
-}
-
+})
 const Course = db.define('courses', {
     name: {
         type: Sequelize.STRING,
